5f918963de07666847a96bccc80163a9bfd7f522
mnorelli <mnorelli360@gmail.com>
2016-05-24 13:41:06 -0700

    in class code along, partially complete

---
+++ reminders/index.js
@@ -0,0 +1,25 @@
+// Dependencies
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// Configuration
+mongoose.connect('mongodb://localhost/reminders');
+process.on('exit', function() { mongoose.disconnect() }); // Shutdown Mongoose correctly
+app.set("view engine", "ejs");  // sets view engine to handlebars
+app.use(bodyParser.json());  // allows for parameters in JSON and html
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));  // looks for assets like stylesheets in a `public` folder
+var port = 3000;  // define a port to listen on
+
+// Controllers
+var remindersController = require("./controllers/remindersController");
+
+// Routes
+app.get("/reminders", remindersController.index);
+
+// Start server
+app.listen(port, function() {
+  console.log("app is running on port:", port);
+});
